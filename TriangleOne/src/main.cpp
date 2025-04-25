218360c0ee4a7574c897413bc7581b8e98337b6e
Cozyvortexa <129335752+Cozyvortexa@users.noreply.github.com>
2025-04-25 02:59:00 +0200

    Base window

--- TriangleOne/src/main.cpp
+++ TriangleOne/src/main.cpp
@@ -2,6 +2,20 @@
 #include <GLFW/glfw3.h>
 #include <iostream> 
 
+int WIDHT = 800;
+int HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
 int main()
 {
 	glfwInit();
@@ -9,7 +23,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(WIDHT, HEIGHT, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -19,12 +33,24 @@ int main()
 
 
 	glfwMakeContextCurrent(window);
-	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-	glViewport(0, 0, 800, 600);
-
-	while (true) {
-
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
 	}
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // Pour adapter le viewport si la fenetre est resize pendant le court du programme 
+	glViewport(0, 0, WIDHT, HEIGHT);
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);  // gere les inputs 
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
 	return 0;
 }
+
