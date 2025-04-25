6a7c974aaabb57987823b4c9249ee8f1dc850529
Cozyvortexa <129335752+Cozyvortexa@users.noreply.github.com>
2025-04-25 02:14:32 +0200

    Hotfix -Change order the order of the creation of the windows
    re

--- TriangleOne/src/main.cpp
+++ TriangleOne/src/main.cpp
@@ -5,6 +5,9 @@
 int main()
 {
 	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
@@ -14,12 +17,10 @@ int main()
 		return -1;
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	glfwMakeContextCurrent(window);
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
 	glViewport(0, 0, 800, 600);
 
 	while (true) {
