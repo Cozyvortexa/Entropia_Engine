95a9bb45e73e9a225672b07a610725c9266068d3
Cozyvortexa <129335752+Cozyvortexa@users.noreply.github.com>
2025-05-27 01:31:40 +0200

    Import Assimp

--- TriangleOne/include/RenderModule.h
+++ TriangleOne/include/RenderModule.h
@@ -20,6 +20,10 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 
 class RenderModule : public Module {
 public:
